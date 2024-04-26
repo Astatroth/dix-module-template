eb99b4e349c994cce79838636ff765072fbb929a
Astatroth <astatroth@gmail.com>
2024-04-26 13:10:42 +0500

    Updated template files

--- vite.config.js
+++ vite.config.js
@@ -3,14 +3,14 @@ import laravel from 'laravel-vite-plugin';
 
 export default defineConfig({
     build: {
-        outDir: '../../public/build-:lc',
+        outDir: '../../public/build-$LOWER_NAME$',
         emptyOutDir: true,
         manifest: true,
     },
     plugins: [
         laravel({
             publicDirectory: '../../public',
-            buildDirectory: 'build-:lc',
+            buildDirectory: 'build-$LOWER_NAME$',
             input: [
                 __dirname + '/resources/assets/sass/app.scss',
                 __dirname + '/resources/assets/js/app.js'
